5b09b7559286a755a3eb39b7ed617d9bb575d729
Greg Mercer (GBear) <gmercer@stanford.edu>
2013-03-26 14:39:06 -0700

    added new enews-signup block

---
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -0,0 +1,14 @@
+<div href="#" class="designed-box cta-enews-signup">
+  <form action="/enews_signup" method="post" id="gsb-public-custom-blocks-enews-signup-form" accept-charset="UTF-8">
+  <div>
+    <div class="form-item form-type-textfield form-item-email">
+      <label for="edit-email">Email address </label>
+      <input type="text" id="edit-email" name="email" value="" size="60" maxlength="128" class="form-text">
+      <div class="description">Please enter your email address.</div>
+    </div>
+  <input type="submit" id="edit-submit" name="op" value="Submit" class="form-submit"><input type="hidden" name="form_build_id" value="form-KUftdGstPdyV6XlBzJ9yoN7qygZxZAEMeA9ING0xQnI">
+  <input type="hidden" name="form_token" value="p2fuCBcOQRxJ0OABaVnwbb4ineymXHrk3vK15jIwB7k">
+  <input type="hidden" name="form_id" value="gsb_public_custom_blocks_enews_signup_form">
+  </div>
+  </form>
+</div>
