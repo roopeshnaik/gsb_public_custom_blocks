2ca44bcf2a5f46b55e6298ef20708ae31c07173d
Greg Mercer <gmercer@stanford.edu>
2013-03-28 07:44:56 -0700

    New styled form code from Kirill

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,12 +1,8 @@
-<div href="#" class="designed-box cta-enews-signup">
-  <form action="/enews_signup" method="post" id="gsb-public-custom-blocks-enews-signup-form" accept-charset="UTF-8">
-  <div>
-    <div class="form-item form-type-textfield form-item-email">
-      <label for="edit-email">Email address </label>
-      <input type="text" id="edit-email" name="email" value="" size="60" maxlength="128" class="form-text">
-      <div class="description">Please enter your email address.</div>
-    </div>
-  <input type="submit" id="edit-submit" name="op" value="Submit" class="form-submit">
-  </div>
-  </form>
+<div href="#" class="designed-box cta-enews-signup">  
+  <form action="/enews_signup" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
+    <label for="edit-email" class="footer__signup__label">Sign Up for out e-News</label>
+    <p class="footer__signup__description">Get the latest business ideas & insights from <em>Stanford Business re:Think</em></p>
+    <input type="text" id="edit-email" placeholder="email adress" name="email" value="" size="60" maxlength="128" class="form-text footer__signup__email">
+    <input type="submit" id="edit-submit" name="op" value="GO" class="form-submit footer__signup__submit">
+  </form>  
 </div>
