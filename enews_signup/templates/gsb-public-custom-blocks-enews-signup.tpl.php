2fa8713e8ad8f065719ea8346d6a740c9b27c0a4
Greg Mercer <gmercer@stanford.edu>
2013-03-29 11:05:10 -0700

    Removed the div. Kirill doesn't need it.

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,8 +1,6 @@
-<div href="#" class="designed-box cta-enews-signup">  
   <form action="/enews_signup" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
     <label for="edit-email" class="footer__signup__label">Sign Up for out e-News</label>
     <p class="footer__signup__description">Get the latest business ideas & insights from <em>Stanford Business re:Think</em></p>
     <input type="text" id="edit-email" placeholder="email adress" name="email" value="" size="60" maxlength="128" class="form-text footer__signup__email">
     <input type="submit" id="edit-submit" name="op" value="GO" class="form-submit footer__signup__submit">
   </form>  
-</div>
