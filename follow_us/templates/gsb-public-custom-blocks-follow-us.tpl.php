3d7da87cd8035444ac17a2a234efc0b1db9239e4
Meghan Palagyi <meghanp@stanford.edu>
2013-04-17 10:33:22 -0700

    Add icon wrapper for social icons.

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -1,7 +1,9 @@
 <div href="#" class="designed-box cta-follow-us">
   <a class="cta-title" href="#">Follow Us<i></i></a>
-  <a href="#" class="circle social-block-twitter"></a>
-  <a href="#" class="circle social-block-youtube"></a>
-  <a href="#" class="circle social-block-linkedin"></a>
-  <a href="#" class="circle social-block-facebook"></a>
+  <div class="icon-wrapper">
+    <a href="#" class="circle social-block-twitter"></a>
+    <a href="#" class="circle social-block-youtube"></a>
+    <a href="#" class="circle social-block-linkedin"></a>
+    <a href="#" class="circle social-block-facebook"></a>
+  </div>
 </div>
