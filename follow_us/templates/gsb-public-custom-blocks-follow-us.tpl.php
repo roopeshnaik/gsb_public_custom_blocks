c20e11691ca2815586e8db56f6bb658e37932fc1
Greg Mercer <gmercer@stanford.edu>
2013-04-17 15:23:22 -0700

    Added links for social sites. Resolves wp-881

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -1,9 +1,9 @@
 <div href="/contact/social" class="designed-box cta-follow-us">
   <a class="cta-title" href="#">Follow Us<i></i></a>
-  <div class="icon-wrapper">
-    <a href="#" class="circle social-block-twitter"></a>
-    <a href="#" class="circle social-block-youtube"></a>
-    <a href="#" class="circle social-block-linkedin"></a>
-    <a href="#" class="circle social-block-facebook"></a>
+  <div class="icon-wrapper">  
+    <a href="https://twitter.com/stanfordbiz" class="circle social-block-twitter"></a>
+    <a href="http://www.youtube.com/stanfordbusiness" class="circle social-block-youtube"></a>
+    <a href="http://www.linkedin.com/groups/Stanford-Graduate-School-Business-GSB-4160574?gid=4160574&trk=hb_side_g" class="circle social-block-linkedin"></a>
+    <a href="https://www.facebook.com/StanfordGSB" class="circle social-block-facebook"></a>
   </div>
 </div>
