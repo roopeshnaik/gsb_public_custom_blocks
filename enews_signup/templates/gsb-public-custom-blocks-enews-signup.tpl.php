f1532055358f8ce10c8db559bdc0f56829750073
Adam Moore <redndahead@gmail.com>
2013-04-19 10:07:15 -0700

    Fix some more typos.

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,6 +1,6 @@
   <form action="/enews_signup" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
     <label for="edit-email" class="footer__signup__label">Sign Up for our e-News</label>
-    <p class="footer__signup__description">Get the latest business ideas & insights from <em>Stanford Business re:Think</em></p>
-    <input type="text" id="edit-email" placeholder="email adress" name="email" value="" size="60" maxlength="128" class="form-text footer__signup__email">
+    <p class="footer__signup__description">Get the latest business ideas &amp; insights from <em>Stanford Business re:Think</em></p>
+    <input type="text" id="edit-email" placeholder="email address" name="email" value="" size="60" maxlength="128" class="form-text footer__signup__email">
     <input type="submit" id="edit-submit" name="op" value="GO" class="form-submit footer__signup__submit">
   </form>  
