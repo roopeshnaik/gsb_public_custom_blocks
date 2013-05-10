7215a01928d8e6802e7e61d5da59441c5559c8ca
Alexei Gorobets <asgorobets@gmail.com>
2013-05-10 15:55:14 -0700

    Add generic last_name filter A-Z links block.

---
+++ js/gsb_feature_faculty_display.js
@@ -0,0 +1,11 @@
+(function ($) {
+ /**
+  * Behaviour for A-Z links block.
+  */
+ Drupal.behaviors.gsb_lastname_az_links = {
+   attach: function (context, settings) {
+     // Code goes here.
+   }
+ }
+
+})(jQuery);
