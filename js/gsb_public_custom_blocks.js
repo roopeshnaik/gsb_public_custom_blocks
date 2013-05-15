dbf3db9586032fb2d717d7bd17c54f1f175405ab
Dumitru Vozian <dumitru.vozian@wearepropeople.com>
2013-05-15 14:47:39 -0700

    Jira task #WP-1240: add anchor functionality

--- js/gsb_public_custom_blocks.js
+++ js/gsb_public_custom_blocks.js
@@ -1,5 +1,17 @@
 Drupal.behaviors.gsbPublicCustomBlocks = {
     attach: function () {
       jQuery('.field-group-tab-wrapper li.vertical-tab-button a').hover(function(){jQuery(this).click();});
+      if (window.location.hash && !Drupal.settings.gsb_public_custom_blocks) {
+        var anchor = window.location.hash;
+        if (!Drupal.settings.gsb_public_custom_blocks) {
+          Drupal.settings.gsb_public_custom_blocks = {};
+        }
+        Drupal.settings.gsb_public_custom_blocks.activated = true;
+        jQuery('.field-group-tab-wrapper li.vertical-tab-button a strong').each(function (index) {
+          if (anchor === '#' + (jQuery(this).text())) {
+            jQuery(this).parent('a').click();
+          }
+        });
+      }
     }
 };
