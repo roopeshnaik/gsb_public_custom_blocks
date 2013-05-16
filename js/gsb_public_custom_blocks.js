57b79bbf7a87d2c0bab7f6513f1c068e6dc06a3a
Dumitru Vozian <dumitru.vozian@wearepropeople.com>
2013-05-16 13:34:13 -0700

    Task #WP-1240: when hover on tabs anchor changes in the url

--- js/gsb_public_custom_blocks.js
+++ js/gsb_public_custom_blocks.js
@@ -1,17 +1,20 @@
 Drupal.behaviors.gsbPublicCustomBlocks = {
     attach: function () {
       jQuery('.field-group-tab-wrapper li.vertical-tab-button a').hover(function(){jQuery(this).click();});
-      if (window.location.hash && !Drupal.settings.gsb_public_custom_blocks) {
-        var anchor = window.location.hash;
-        if (!Drupal.settings.gsb_public_custom_blocks) {
-          Drupal.settings.gsb_public_custom_blocks = {};
-        }
-        Drupal.settings.gsb_public_custom_blocks.activated = true;
-        jQuery('.field-group-tab-wrapper li.vertical-tab-button a strong').each(function (index) {
-          if (anchor === '#' + (jQuery(this).text())) {
+      jQuery('.field-group-tab-wrapper li.vertical-tab-button a').click(function(){window.location.hash = jQuery(this).find('strong').text()});
+      jQuery('.field-group-tab-wrapper li.vertical-tab-button a strong').each(function (index) {
+        var text = jQuery(this).text();
+        jQuery(this).parent('a').attr('href', '#' + text);
+        if (window.location.hash && !Drupal.settings.gsb_public_custom_blocks) {
+          //open tab if anchor present
+          var anchor = window.location.hash;
+          anchor = anchor.replace('%20', ' ');
+          if (anchor === '#' + text) {
             jQuery(this).parent('a').click();
           }
-        });
-      }
+        }
+      });
+      Drupal.settings.gsb_public_custom_blocks = {};
+      Drupal.settings.gsb_public_custom_blocks.activated = true;
     }
 };
