4e1bf70e23a3c6aa08214c28f759415149dda794
Dumitru Vozian <dumitru.vozian@wearepropeople.com>
2013-05-16 22:31:35 -0700

    Task #WP-1240: add scroll to program finder if anchor in url

--- js/gsb_public_custom_blocks.js
+++ js/gsb_public_custom_blocks.js
@@ -1,8 +1,8 @@
 Drupal.behaviors.gsbPublicCustomBlocks = {
     attach: function () {
-      jQuery('.field-group-tab-wrapper li.vertical-tab-button a').hover(function(){jQuery(this).click();});
-      jQuery('.field-group-tab-wrapper li.vertical-tab-button a').click(function(){window.location.hash = jQuery(this).find('strong').text()});
-      jQuery('.field-group-tab-wrapper li.vertical-tab-button a strong').each(function (index) {
+      jQuery('div.pane-program-finder .field-group-tab-wrapper li.vertical-tab-button a').hover(function(){jQuery(this).click();});
+      jQuery('div.pane-program-finder .field-group-tab-wrapper li.vertical-tab-button a').click(function(){window.location.hash = jQuery(this).find('strong').text()});
+      jQuery('div.pane-program-finder .field-group-tab-wrapper li.vertical-tab-button a strong').each(function (index) {
         var text = jQuery(this).text();
         jQuery(this).parent('a').attr('href', '#' + text);
         if (window.location.hash && !Drupal.settings.gsb_public_custom_blocks) {
@@ -11,6 +11,9 @@ Drupal.behaviors.gsbPublicCustomBlocks = {
           anchor = anchor.replace('%20', ' ');
           if (anchor === '#' + text) {
             jQuery(this).parent('a').click();
+            jQuery('div.pane-program-finder').attr('id', 'pane-program-finder');
+            window.location.hash = 'pane-program-finder';
+            window.location.hash = text;
           }
         }
       });
