4e3d2f35adae30492c114e1bc8966a35fdf02293
Dumitru Vozian <dumitru.vozian@wearepropeople.com>
2013-05-23 14:03:19 -0700

    Task #WP-1473: added the correct url to href attribute

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -1,5 +1,5 @@
-<div href="/contact/social" class="designed-box cta-follow-us">
-  <a class="cta-title" href="#">Follow Us<i></i></a>
+<div class="designed-box cta-follow-us">
+  <a class="cta-title" href="/contact/social">Follow Us<i></i></a>
   <div class="icon-wrapper">
     <a href="https://twitter.com/stanfordbiz" target="_blank" class="circle social-block-twitter"></a>
     <a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"></a>
