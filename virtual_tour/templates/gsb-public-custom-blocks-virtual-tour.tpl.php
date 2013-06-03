a63d378a2037e92486e2e0059b3aa61fdb12108f
Greg Mercer <gmercer@stanford.edu>
2013-06-03 12:13:19 -0600

    Changed to a new link for the virtual tour.

--- virtual_tour/templates/gsb-public-custom-blocks-virtual-tour.tpl.php
+++ virtual_tour/templates/gsb-public-custom-blocks-virtual-tour.tpl.php
@@ -1,4 +1,4 @@
-<a href="/visit/virtual-tour" class="designed-box cta-virtual-tour">
+<a href="/visit/closer-look-campus" class="designed-box cta-virtual-tour">
   <span class="virtual-tour-btn">Take a Closer Look<i></i></span>
   <div class="virtual-tour-cover"></div>
 </a>
