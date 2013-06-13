8f69035f8b24aa717a47fce859e2a63401a8b50a
Adam Moore <redndahead@gmail.com>
2013-06-13 14:51:14 -0700

    Use class instead of id to target the program finder.

--- js/gsb_public_custom_blocks.js
+++ js/gsb_public_custom_blocks.js
@@ -27,8 +27,8 @@
 
         // Add our responsive javascript.
         if (Modernizr.mq('(max-width: 768px)')) {
-          $('#pane-program-finder .vertical-tabs-list').addClass('resp-tabs-list');
-          $('#pane-program-finder .vertical-tabs').easyResponsiveTabs({type: 'accordion'});
+          $('.pane-program-finder .vertical-tabs-list').addClass('resp-tabs-list');
+          $('.pane-program-finder .vertical-tabs').easyResponsiveTabs({type: 'accordion'});
         }
       }
   };
