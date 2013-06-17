3459821f6eb72b701edbb80a0f737fa00343ec8b
Maia Morosanu <maia.morosanu@wearepropeople.com>
2013-06-17 11:53:31 -0700

    WP-1555 changed around globe block category to preformatted CTA

---
+++ around_globe/templates/gsb-public-custom-blocks-around-globe.tpl.php
@@ -0,0 +1,7 @@
+<div class="designed-box cta-around-globe">
+  <a href="#" class="around-globe-link">
+    <div class="link-inner">
+      <div class="link-title">GSB Around <br /> the Globe</div>
+    </div>
+  </a>
+</div>
