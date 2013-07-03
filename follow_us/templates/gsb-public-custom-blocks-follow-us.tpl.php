86b988657880c5e7e9f7fce750ad4608120f65f8
Greg Mercer <gmercer@stanford.edu>
2013-07-03 13:56:34 -0700

    WP-1832 Changed href for linkedin link on Follow Us CTA.

--- follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
+++ follow_us/templates/gsb-public-custom-blocks-follow-us.tpl.php
@@ -3,7 +3,7 @@
   <div class="icon-wrapper">
     <a href="https://twitter.com/stanfordbiz" target="_blank" class="circle social-block-twitter"></a>
     <a href="http://www.youtube.com/stanfordbusiness" target="_blank" class="circle social-block-youtube"></a>
-    <a href="http://www.linkedin.com/groups/Stanford-Graduate-School-Business-GSB-4160574?gid=4160574&trk=hb_side_g" target="_blank" class="circle social-block-linkedin"></a>
+    <a href="http://www.linkedin.com/company/stanford-graduate-school-of-business" target="_blank" class="circle social-block-linkedin"></a>
     <a href="https://www.facebook.com/StanfordGSB" target="_blank" class="circle social-block-facebook"></a>
   </div>
 </div>
