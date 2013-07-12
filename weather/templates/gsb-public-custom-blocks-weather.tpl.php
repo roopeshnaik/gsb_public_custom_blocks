c4db758315a6d0cde2d7704d2aec0dc8752f4bdd
Alexei Gorobets <asgorobets@gmail.com>
2013-07-12 12:02:31 +0300

    WP-1880: Changes to Weather widget.

--- weather/templates/gsb-public-custom-blocks-weather.tpl.php
+++ weather/templates/gsb-public-custom-blocks-weather.tpl.php
@@ -1,1 +1,8 @@
-<div class="weather-widget"><?php print $content; ?></div>
+<div class="weather-widget">
+  <h4>Currently in Stanford, CA</h4>
+  <?php print $image; ?>
+  <strong><?php print $temp; ?></strong>
+  <p class="more">
+     <a href="http://weather.stanford.edu" target="_blank">STANFORD STATION »</a>
+  </p>
+</div>
