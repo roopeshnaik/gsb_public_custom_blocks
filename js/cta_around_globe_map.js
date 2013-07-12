3e6cd16e55d1017427dc5495916c87393e15b945
Greg Mercer (GBear) <gmercer@stanford.edu>
2013-07-12 07:42:47 -0700

    WP-1864 Added a catch block to handle when the map is first added

--- js/cta_around_globe_map.js
+++ js/cta_around_globe_map.js
@@ -1,8 +1,11 @@
 (function ($) {
   Drupal.behaviors.ctaAroundGlobeMap = {
     attach: function () {
-	    var map = L.mapbox.map("map", "jennyhutch.GSB_test8,jennyhutch.GSBregions");
-	    map.gridControl.options.sanitizer = function(x) { return x; };
+    	try {
+    		var map = L.mapbox.map("map", "jennyhutch.GSB_test8,jennyhutch.GSBregions");
+	    	map.gridControl.options.sanitizer = function(x) { return x; };    	
+    	} catch(e) {
+    	}
     }
   };
 })(jQuery);
