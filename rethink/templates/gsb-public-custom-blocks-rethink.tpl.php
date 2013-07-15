94147ffe7b688ae6e80812ddb95725a35b06b9de
Maia Morosanu <maia.morosanu@wearepropeople.com>
2013-07-15 15:48:04 -0700

    WPBQA-252 title spacing

--- rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
+++ rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
@@ -1,6 +1,6 @@
 <a href="http://stanford.us5.list-manage2.com/subscribe?u=ce785d9b9016cd35fb68e83b7&id=0b5214e34" class="designed-box cta-rethink">
   <p class="cta-title">
-    <b>RE:</b>THINK
+    <b>RE: </b>THINK
   </p>
   <p class="cta-description">
     Get the latest business ideas & insights.<b>Sign up for our e-News</b><i></i>
