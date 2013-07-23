c4f4389d40155c8aeeb2a57e3e0dcf2d6e733a5f
Greg Mercer <gmercer@stanford.edu>
2013-07-23 11:01:48 -0700

    WPBQA-278 Changed link and text

--- rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
+++ rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
@@ -1,8 +1,8 @@
-<a href="http://stanford.us5.list-manage2.com/subscribe?u=ce785d9b9016cd35fb68e83b7&id=0b5214e34" class="designed-box cta-rethink">
+<a href="http://eepurl.com/q-JMr" class="designed-box cta-rethink">
   <p class="cta-title">
     <b>RE: </b>THINK
   </p>
   <p class="cta-description">
-    Get the latest business ideas & insights.<b>Sign up for our e-News</b><i></i>
+    Get the latest business ideas & insights.<b> Sign up for our eNews</b><i></i>
   </p>
 </a>
