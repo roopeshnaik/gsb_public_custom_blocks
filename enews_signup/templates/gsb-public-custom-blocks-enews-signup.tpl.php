a37a35c8336eefb34f60db59e19f1b60f1813ed8
Greg Mercer <gmercer@stanford.edu>
2013-07-23 09:59:41 -0700

    WP-1876 Added link to the /contact/rethink page.

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,5 +1,5 @@
   <form action="/enews_signup" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
-    <label for="edit-email" class="footer__signup__label">Sign Up for our e-News</label>
+    <label for="edit-email" class="footer__signup__label"><a href="/contact/rethink">Sign Up for our e-News</a></label>
     <p class="footer__signup__description">Get the latest business ideas &amp; insights from <em>Stanford Business re:Think</em></p>
     <input type="text" id="edit-email" placeholder="email address" name="email" value="" size="60" maxlength="128" class="form-text footer__signup__email">
     <input type="submit" id="edit-submit" name="op" value="GO" class="form-submit footer__signup__submit">
