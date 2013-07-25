f829d48cdd9da0e7ca4299321bc6dea496190746
Greg Mercer <gmercer@stanford.edu>
2013-07-25 08:12:13 -0700

    WP-1828 Text and link changes

--- enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
+++ enews_signup/templates/gsb-public-custom-blocks-enews-signup.tpl.php
@@ -1,6 +1,6 @@
   <form action="/enews_signup" method="post" id="gsb-enews-signup-form" accept-charset="UTF-8" class="footer__signup">
-    <label for="edit-email" class="footer__signup__label"><a href="/contact/rethink">Sign Up for our e-News</a></label>
-    <p class="footer__signup__description">Get the latest business ideas &amp; insights from <em>Stanford Business re:Think</em></p>
+    <label for="edit-email" class="footer__signup__label"><a href="/contact/rethink">Sign Up for Our eNews</a></label>
+    <p class="footer__signup__description">Get the latest business ideas and insights from <em>Stanford Business Re:Think</em></p>
     <input type="text" id="edit-email" placeholder="email address" name="email" value="" size="60" maxlength="128" class="form-text footer__signup__email">
     <input type="submit" id="edit-submit" name="op" value="GO" class="form-submit footer__signup__submit">
-  </form>  
+  </form> 
