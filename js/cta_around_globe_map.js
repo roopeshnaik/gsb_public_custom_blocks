c84f4021fe115286e237dec762e9d23c7ac82102
Greg Mercer (GBear) <gmercer@stanford.edu>
2013-07-30 15:10:38 -0700

    WPBQA-308 Changed js and css per Jens request

--- js/cta_around_globe_map.js
+++ js/cta_around_globe_map.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.ctaAroundGlobeMap = {
     attach: function () {
     	try {
-    		var map = L.mapbox.map("map", "jennyhutch.GSB_test8,jennyhutch.GSBregions");
+    		var map = L.mapbox.map("map", "jennyhutch.gsb-main-data");
 	    	map.gridControl.options.sanitizer = function(x) { return x; };    	
     	} catch(e) {
     	}
