233bc0b41b9f33909775f04eed6c0674f31e4be6
Greg Mercer <gmercer@stanford.edu>
2013-08-01 13:08:56 -0700

    WPBQA-329 Removed space between RE:THINK

--- rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
+++ rethink/templates/gsb-public-custom-blocks-rethink.tpl.php
@@ -1,6 +1,6 @@
 <a href="http://eepurl.com/q-JMr" class="designed-box cta-rethink">
   <p class="cta-title">
-    <b>RE: </b>THINK
+    <b>RE:</b>THINK
   </p>
   <p class="cta-description">
     Get the latest business ideas and insights.<b> Sign up for our eNews</b><i></i>
