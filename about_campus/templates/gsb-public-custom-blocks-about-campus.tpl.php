60bdc7f5d87feac2ff02db58a87748fd532b6321
Greg Mercer <gmercer@stanford.edu>
2013-08-15 10:02:21 -0700

    WPBQA-370 Changed link.

--- about_campus/templates/gsb-public-custom-blocks-about-campus.tpl.php
+++ about_campus/templates/gsb-public-custom-blocks-about-campus.tpl.php
@@ -1,4 +1,4 @@
-<a href="/stanford-gsb-experience/life-stanford/campus" class="learn-about-campus">
+<a href="/stanford-gsb-experience/life-stanford-gsb/campus" class="learn-about-campus">
   <span class="about-campus-btn">Learn About<br /> Our Campus</span>
   <div class="about-campus-cover"></div>
 </a>
